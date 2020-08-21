2c23dea909f737b79c73ec1813d49e3b354a8e2a
S. M. Jubayer <sadatjubayer@gmail.com>
2020-08-21 20:24:18 +0600

    new blades

---
+++ resources/views/home/delete.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Delete User</title>
+</head>
+<body>
+
+	<h1>Confirm Deletion</h1>
+
+	<form method="post">
+        <h2>Are you sure to delet the user?</h2>
+        <button type="submit">Confirm</button>
+	</form>
+</body>
+</html>
